93de70856adb8472f0a945da6d93134156983aa0
lirentian0215 <83382249+lirentian0215@users.noreply.github.com>
2023-04-09 16:56:23 +0800

    Update main.go

--- cmd/dashboard/main.go
+++ cmd/dashboard/main.go
@@ -1,41 +1,56 @@
+package main
+
 import (
-    "...."
-        "...."
-        "os"
-    "fmt"
-    "...."
-        "...."
-        "...."
+	"context"
+	"log"
+
+	"github.com/naiba/nezha/cmd/dashboard/controller"
+	"github.com/naiba/nezha/cmd/dashboard/rpc"
+	"github.com/naiba/nezha/model"
+	"github.com/naiba/nezha/service/singleton"
+	"github.com/ory/graceful"
 )
-....
-....
+
 func init() {
+	// 初始化 dao 包
+	singleton.InitConfigFromPath("data/config.yaml")
+	singleton.InitTimezoneAndCache()
+	singleton.InitDBFromPath("data/sqlite.db")
+	singleton.InitLocalizer()
+	initSystem()
+}
 
-    //新建conf文件
-    file6, err := os.Create("data/config.yaml")
-    if err != nil {
-        fmt.Println(err)
-    }
-    data := `debug: false
-httpport: 80
-grpcport: 7777
-oauth2:
-  type: "github" #Oauth2 登录接入类型，gitee/github
-  admin: "lirentian0215" #管理员列表，半角逗号隔开
-  clientid: "313c39e6e931afbe5fa5" # 在 https://github.com/settings/developers 创建，无需审核 Callback 填 http(s)://域名或IP/oauth2/callback
-  clientsecret: "f977ace0528359ff81185d6edf53fee52a5d848d"
-site:
-  brand: "名字"
-  cookiename: "nezha-dashboard" #浏览器 Cookie 字段名，可不改
-  theme: "default"
-`
-    file6.WriteString(data)
-    file6.Close()
-    // 初始化 dao 包
+func initSystem() {
+	// 启动 singleton 包下的所有服务
+	singleton.LoadSingleton()
 
-    singleton.Init()
-    singleton.InitConfigFromPath("data/config.yaml")
-    singleton.InitDBFromPath("data/sqlite.db")
-    singleton.InitLocalizer()
-    initSystem()
+	// 每天的3:30 对 监控记录 和 流量记录 进行清理
+	if _, err := singleton.Cron.AddFunc("0 30 3 * * *", singleton.CleanMonitorHistory); err != nil {
+		panic(err)
+	}
+
+	// 每小时对流量记录进行打点
+	if _, err := singleton.Cron.AddFunc("0 0 * * * *", singleton.RecordTransferHourlyUsage); err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	singleton.CleanMonitorHistory()
+	go rpc.ServeRPC(singleton.Conf.GRPCPort)
+	serviceSentinelDispatchBus := make(chan model.Monitor) // 用于传递服务监控任务信息的channel
+	go rpc.DispatchTask(serviceSentinelDispatchBus)
+	go rpc.DispatchKeepalive()
+	go singleton.AlertSentinelStart()
+	singleton.NewServiceSentinel(serviceSentinelDispatchBus)
+	srv := controller.ServeWeb(singleton.Conf.HTTPPort)
+	graceful.Graceful(func() error {
+		return srv.ListenAndServe()
+	}, func(c context.Context) error {
+		log.Println("NEZHA>> Graceful::START")
+		singleton.RecordTransferHourlyUsage()
+		log.Println("NEZHA>> Graceful::END")
+		srv.Shutdown(c)
+		return nil
+	})
 }
